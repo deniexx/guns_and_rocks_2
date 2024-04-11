4093e8b3bac61e64d7fd1053b3dd61ab9fd3bab9
Denis Dimitrov <deniexxx17@gmail.com>
2024-04-11 16:25:52 +0100

    RotateAngleAxis

--- Assets/Scripts/GameplayStatics.cs
+++ Assets/Scripts/GameplayStatics.cs
@@ -17,4 +17,30 @@ public static class GameplayStatics
         
         return new Vector2(direction.x, direction.y);
     }
+
+    public static Vector3 RotateAngleAxis(float angle, Vector3 axis, Vector3 vector)
+    {
+        float sin = Mathf.Sin(angle * Mathf.Deg2Rad);
+        float cos = Mathf.Sin(angle * Mathf.Deg2Rad);
+
+        float xx = axis.x * axis.x;
+        float yy = axis.y * axis.y;
+        float zz = axis.z * axis.z;
+                    
+        float xy = axis.x * axis.y;
+        float yz = axis.y * axis.z;
+        float zx = axis.z * axis.x;
+                    
+        float xs = axis.x * sin;
+        float ys = axis.y * sin;
+        float zs = axis.z * sin;
+
+        float omc = 1f - cos;
+
+        return new Vector3(
+            (omc * xx + cos) * vector.x + (omc * xy - zs) * vector.y + (omc * zx + ys) * vector.z,
+            (omc * xy + zx) * vector.x + (omc * yy + cos) * vector.y + (omc * yz - xs) * vector.z,
+            (omc * zx - ys) * vector.x + (omc * yz + xs) * vector.y + (omc * zz + cos) * vector.z
+        );
+    }
 }
