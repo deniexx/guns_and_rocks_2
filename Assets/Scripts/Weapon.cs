3399e35fe1efb6296c3c4e5bd3bd2124ac64d8e0
TorinPellegrini <2102892@uad.ac.uk>
2024-04-11 17:10:50 +0100

    Weapon firing

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Sprite currentWeaponSpr;
+    public GameObject bulletPrefab;
+
+    public float fireRate = 1;
+
+    public int magazineAmmo = 6;
+    public int totalAmmo = 32;
+    public int currentAmmo = 32;
+    public int damage = 20;
+    public int projectileSpreadAngle = 90;
+    public int numOfProjectiles = 5;
+    public int recoilImpulse = 0;
+
+    private bool bIsFiring = true;
+
+    private float nextTimeOfFire = 0;
+    public void StartFiring()
+    {
+        Debug.Log("Weapon firing!");
+        bIsFiring = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (bIsFiring == true)
+        {
+            if (Time.time >= nextTimeOfFire)
+            {
+                Vector3 bulletDir = -GameplayStatics.GetDirectionFromMouseToLocation(transform.position);
+                Vector3 leftOfSpread = GameplayStatics.RotateVector(bulletDir, -projectileSpreadAngle / 2f);
+
+                float deltaSpread = projectileSpreadAngle / (float)(numOfProjectiles + 1);
+                for (int i = 0; i < numOfProjectiles; i++)
+                {
+                    Vector3 dirActual = GameplayStatics.RotateVector(leftOfSpread, deltaSpread * i);
+
+                    //Debug.DrawLine(transform.position, transform.position + dirActual * 20, Color.black, 5f);
+
+                    GameObject bulletGO = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                    Bullet bullet = bulletGO.GetComponent<Bullet>();
+                    bullet.damage = damage;
+                    bullet.dir = dirActual;
+                    nextTimeOfFire = Time.time + 1 / fireRate;
+                }
+
+            }
+
+        }
+    }
+
+    public void EndFiring()
+    {
+        Debug.Log("Weapon stopped firing!");
+        bIsFiring = false;
+    }
+
+}
